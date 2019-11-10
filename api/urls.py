8a56518385b107198289e90fc29035751d2bc06f
Konstrictor <jonkur96@gmail.com>
2019-11-10 22:09:17 +0200

    release version

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,8 @@ router.register('materiels', views.MaterielViewSet)
 
 urlpatterns = [
 	path('', include(router.urls)),
+	path('get_auto/<plaque>', views.get_auto, name='get_auto'),
+	path('get_client/<CNI>', views.get_client, name='get_client'),
 	path('login', views.login, name='login'),
 	path('logout', views.logout, name='logout')
 ]
